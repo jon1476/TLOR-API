e5782f8f169a89d704933aa67d1aff509bacecd7
Jon Aniz <aniz.153990@e.unavarra.es>
2025-05-12 18:25:55 +0200

    Vardump para comprobar el valor de uri

--- public/index.php
+++ public/index.php
@@ -9,6 +9,8 @@ header("Access-Control-Allow-Headers: Content-Type");
 
 $method = $_SERVER['REQUEST_METHOD'];
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+var_dump($uri); exit;
+
 
 $conn = connectToDB();
 
